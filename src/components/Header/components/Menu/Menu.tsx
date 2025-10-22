631867042e18c3296c51ac19e912be46839f288b
Leo Oliveira <leo2281999jose@gmail.com>
2025-10-21 23:34:31 -0300

    estrutura do menu part 1 - sem scss

--- src/components/Header/components/Menu/Menu.tsx
+++ src/components/Header/components/Menu/Menu.tsx
@@ -1,8 +1,40 @@
+import Logo from "../../../../assets/logo/Logo.jsx"
+import SearchIcon from "../../../../assets/icon/SearchIcon.jsx"
+import BoxIcon from "../../../../assets/icon/BoxIcon.jsx"
+import HeartIcon from "../../../../assets/icon/HeartIcon.jsx"
+import UserIcon from "../../../../assets/icon/UserIcon.jsx"
+import ShopCartIcon from "../../../../assets/icon/ShopCartIcon.jsx"
+import "./Menu.scss"
+
+
+
 function Menu() {
 
   return (
-    <div>
-      
+    <div className="Menu-container">
+      <div className="Logo-container">
+        <Logo />
+      </div>
+      <div className="search-container">
+        <input type="text" placeholder="O que você está buscando?" />
+        <span className="search-icon">
+          <SearchIcon />
+        </span>
+      </div>
+      <div>
+        <div>
+          <BoxIcon />
+        </div>
+        <div>
+          <HeartIcon />
+        </div>
+        <div>
+          <UserIcon />
+        </div>
+        <div>
+          <ShopCartIcon />
+        </div>
+      </div>
     </div>
   )
 }
