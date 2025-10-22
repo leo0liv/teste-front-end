0a155454b3e58e95a6183ac7b38ab9f6a8318db1
Leo Oliveira <leo2281999jose@gmail.com>
2025-10-22 18:05:42 -0300

    navbar part 1

--- src/components/Header/components/Navbar/Navbar.tsx
+++ src/components/Header/components/Navbar/Navbar.tsx
@@ -1,7 +1,21 @@
+import CrownIcon from "../../../../assets/icon/CrownIcon.jsx"
+import "./Navbar.scss"
+
 function Navbar() {
 
   return (
-    <nav>
+    <nav className="Navbar">
+      <div className="Navbar-container">
+        <ul className="Navbar-categories">
+                <li><a href="#" className="Navbar-text">TODAS CATEGORIAS</a></li>
+                <li><a href="#" className="Navbar-text">SUPERMERCADO</a></li>
+                <li><a href="#" className="Navbar-text">LIVROS</a></li>
+                <li><a href="#" className="Navbar-text">MODA</a></li>
+                <li><a href="#" className="Navbar-text">LANÇAMENTOS</a></li>
+                <li><a href="#" className="Navbar-text"><span className="Color-oferta">OFERTAS DO DIA</span></a></li>
+                <li><a href="#" className="Navbar-text"><CrownIcon/>&nbsp;ASSINATURA</a></li>
+        </ul>
+      </div>
       
     </nav>
   )
